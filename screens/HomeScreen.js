d03b024f4aeb45211bf5bc8ffaf5d1a0f2126172
SpyGuy <shashankprasanna1@gmail.com>
2021-03-28 20:20:10 -0400

    added States

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import {Header} from 'react-native-elements'; 
 
 export default class HomeScreen extends React.component{
+    constructor() {
+        super(); 
+        this.state={
+            text: " ",
+            isSearchPressed: false, 
+            word: " ", 
+            lexicalCategory: " ", 
+            examples: [], 
+            definition: ""
+        }
+    }
+
     render() {
         return(
-            <View>
-                <Text>Dictionary</Text>
+            <View style={styles.containerStyle}>
+                <Header>Dictionary</Header>
+                <TextInput onChangeText={text => {
+                    this.setState({
+                        text: text,
+                        isSearchPressed: false, 
+                        word: " ", 
+                        lexicalCategory: " ", 
+                        examples: [], 
+                        definition: ""
+                    })
+                }}></TextInput>
             </View>
         )
     }
 }
 const styles = StyleSheet.create({
+    containerStyle:{
+        flex:1,
+        backgroundColor: "#121212",
+        alignContent: "center"
+    },
     textStyle:{
-        color: "white"
+        textAlign: "center",
+        color: "white",
     }
 })
